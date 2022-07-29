342370400e7d2374ab3c87fd1876dcf8fa5c6ea9
realdarkxeon <realdarkxeon@gmail.com>
2022-07-29 15:54:45 +0600

    build counting process (timer) for every website with an idle time of 30s

--- background.js
+++ background.js
@@ -1,97 +1,84 @@
+let UPDATE_INTERVAL = 1;
+let DOMAINS_LIMIT = 10;
+let TYPE = {
+    "today": "today",
+    "average": "average",
+    "overall": "overall"
+};
+
+// let mode = TYPE.today;
+
+function toFormalDate(obj) {
+    return `${obj.getFullYear()}-${obj.getMonth()+1}-${obj.getDate()}`;
+}
+
 const getHostname = (url) => {
     return new URL(url).hostname;
 }
 
-let lastUrl = "", badgeTimer, url;
-
-const get_data = (temp_url) => {
+const get_data = (item) => {
     return new Promise((resolve, reject) => {
-        chrome.storage.sync.get([temp_url], (res) => {
-            resolve(res);
+        chrome.storage.sync.get([item], (res) => {
+            resolve(res[item]);
         });
     });
 };
 
-const get_time = (seconds) => {
-    let answer = "";
-    if(seconds < 60) {
-        answer = `${seconds}s`;
+async function restart() {
+    let days_count = await get_data("days_count");
+    if(!days_count) {
+        chrome.storage.sync.set({"days_count": 1});
     }
-    else if(seconds < 3600) {
-        answer = `${Math.floor(seconds / 60)}m`;
+    let date = await get_data("date");
+    if(!date) {
+        chrome.storage.sync.set({"date": toFormalDate(new Date())});
     }
-    else {
-        answer = `${Math.floor(seconds / 3600)}h`;
+    let domains = await get_data("domains");
+    if(!domains) {
+        chrome.storage.sync.set({"domains": JSON.stringify({})});
     }
-    return answer;
-};
+}
 
-chrome.tabs.onActivated.addListener(() => {
-    chrome.tabs.query({active: true, status: "complete", lastFocusedWindow: true}, async (res) => {
-        url = getHostname(res[0].url);
+async function processLastTab(tabs) {
+    if(tabs.length === 0) {
+        return;
+    }
+    let tab = tabs[0];
+    let today = toFormalDate(new Date());
+    let domain = getHostname(tab.url);
+    let domains = JSON.parse(await get_data("domains"));
+    if(!(domain in domains)) {
+        domains[domain] = {
+            "alltime": 0,
+            "days": {}
+        };
+    }
+    if(!domains[domain].days[today]) {
+        domains[domain].days[today] = 0;
+    }
+    domains[domain].days[today] += UPDATE_INTERVAL;
+    domains[domain].alltime += UPDATE_INTERVAL;
+    chrome.storage.sync.set({"domains": JSON.stringify(domains)});
+    let num_min = Math.floor(domains[domain].days[today] / 60).toString();
+    if(num_min.length < 4) {
+        num_min += "m";
+    }
+    await chrome.action.setBadgeText({text: num_min});
+}
 
-        chrome.storage.sync.set({[url]: {
-            "seconds": 0,
-            "visit": (new Date()).toJSON()
-        }});
-
-        if(badgeTimer) clearInterval(badgeTimer);
-
-        if(lastUrl !== "" && lastUrl !== url) {
-            let current = await get_data(url);
-            let last = await get_data(lastUrl);
-            
-            last[lastUrl].seconds += Math.floor((new Date(current[url].visit) - new Date(last[lastUrl].visit)) / 1000);
-            
-            chrome.storage.sync.set({[lastUrl]: {
-                "seconds": last[lastUrl].seconds,
-                "visit": last[lastUrl].visit
-            }});
-            
-            last = await get_data(lastUrl);
-            console.log(last);
-            
-            // TODO: start timer on the badge
-
-            let displayTime = current[url].seconds;
-
-            badgeTimer = setInterval(() => {
-                chrome.action.setBadgeText({text: get_time(displayTime)});
-                displayTime += 3;
-            }, 3000);
+function updateData() {
+    chrome.idle.queryState(30, function(state) {
+        if(state === "active") {
+            chrome.tabs.query({lastFocusedWindow: true, active: true}, function(tabs) {
+                processLastTab(tabs);
+            });
         }
-        lastUrl = url;
     });
-}); 
+}
 
-let windowStart, windowEnd;
-const handleBrowserFocus = async (windowId) => {
-    if(windowStart && windowId !== chrome.windows.WINDOW_ID_NONE) {
-        windowEnd = new Date();
-        // TODO: subtract idle time from webtime
-        let current = await get_data(url);
-        current[url].seconds -= Math.floor((new Date(windowEnd) - new Date(windowStart)) / 1000);
-        chrome.storage.sync.set({[url]: {
-            "seconds": current[url].seconds,
-            "visit": current[url].visit
-        }});
-        windowStart = null;
-        
-        let displayTime = current[url].seconds;
-
-        badgeTimer = setInterval(() => {
-            chrome.action.setBadgeText({text: get_time(displayTime)});
-            displayTime += 3;
-        }, 3000);
-    }
-    else if(windowId === chrome.windows.WINDOW_ID_NONE) {
-        windowStart = new Date();
-        // TODO: stop timer on the badge
-        clearInterval(badgeTimer);
-    }
-};
-
-chrome.windows.onFocusChanged.addListener((windowId) => {
-    handleBrowserFocus(windowId);
+let timerId = 0;
+chrome.runtime.onInstalled.addListener(() => {
+    if(timerId) clearInterval(timerId);
+    restart();
+    timerId = setInterval(updateData, UPDATE_INTERVAL * 1000);
 });
-
